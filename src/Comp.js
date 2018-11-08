d9a169bec7f152c354dda4f53e62a168ada50d9e
ShaneKing <github@azolla.org>
2018-11-08 22:50:19 +0800

    fixed #2 skTransProps2Self&skTransProps2Child just trans sk.../ks... props

--- src/Comp.js
+++ src/Comp.js
@@ -389,30 +389,21 @@ export default class Comp extends React.Component {
   }
 
   /**
-   * Valid props for this comp
-   *
-   * @param {Comp|string} comp not div, just have 'div' and Div or React Component etc...
-   * @param {object} prop
-   * @param {boolean} htmlProps @Deprecated at antd 3.8.1 used in FormComp.render at antd 3.5.4
-   * @returns {object}
+   * 1. trans prop start with sk
+   * 2. trans all prop to "self"(like SKInput for Input, SKFormInput to SKInput)
    */
   skTransProps2Self(comp = this.props.compTag, prop = this.props) {
     const skProps = Object.keys(prop.skFilter(false, Comp.skPropsFilter));
     const ksProps = Object.keys(prop.skFilter(false, Comp.ksPropsFilter));
 
     let allowProps = skProps;
-    if (this.SK_COMP_NAME) {
-      if (comp.NON_SK_COMP_NAME && (Comp.SK_PROPS_PREFIX.toUpperCase() + comp.NON_SK_COMP_NAME) === this.SK_COMP_NAME) {
-        //SK COMP to AntD
-        allowProps = allowProps.concat(Reacts.P.skVals());
-      }
-      if (this.SK_EXTEND_COMP_NAME && comp.SK_COMP_NAME === this.SK_COMP_NAME) {
-        //SKFormInput to SKInput
-        allowProps = allowProps.concat(Reacts.P.skVals());
-      }
-    }
     allowProps = Reacts.TAG[comp] ? SK.s4a(Reacts.TP[comp]) : allowProps.concat(ksProps);//if html node, nothing to do
-    allowProps = comp.propTypes ? allowProps.concat(Object.keys(comp.propTypes)) : allowProps;
+    if (this.SK_COMP_NAME
+      && ((comp.NON_SK_COMP_NAME && (Comp.SK_PROPS_PREFIX.toUpperCase() + comp.NON_SK_COMP_NAME) === this.SK_COMP_NAME)
+        || (this.SK_EXTEND_COMP_NAME && comp.SK_COMP_NAME === this.SK_COMP_NAME))) {
+      allowProps = allowProps.concat(Reacts.P.skVals());
+      allowProps = comp.propTypes ? allowProps.concat(Object.keys(comp.propTypes)) : allowProps;
+    }
     allowProps = allowProps.concat(this.allowTransProps2Self(comp, prop));
 
     let denyProps = [Comp.SK_PROPS.COMP_TAG, Comp.SK_PROPS.MODEL_ID];//force ignore props
