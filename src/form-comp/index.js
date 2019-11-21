5e64c9377b650815ef4ce6b93cddaeb8071f51c4
ShaneKing <github@azolla.org>
2019-11-21 22:51:34 +0800

    upgrade dep

--- src/form-comp/index.js
+++ src/form-comp/index.js
@@ -1,6 +1,5 @@
 export FormComp from './FormComp';
 export SKFormDatePicker from './SKFormDatePicker';
-export SKFormGoJSDag from './SKFormGoJSDag';
 export SKFormInput from './SKFormInput';
 export SKFormInputGroup from './SKFormInputGroup';
 export SKFormRangePicker from './SKFormRangePicker';
